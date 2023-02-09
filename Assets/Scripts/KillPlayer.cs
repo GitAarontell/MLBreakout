3891e72a0d9a737301bb03773071a617d22bd0ef
hamrangj <hamrangj@oregonstate.edu>
2023-02-08 23:15:46 -0800

    Corrected some issues my merging caused with respawn
    Also started the game over script to provide the player access back to the menu, but saw the time. Ill continue tomorrow

--- Assets/Scripts/KillPlayer.cs
+++ Assets/Scripts/KillPlayer.cs
@@ -1,10 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
-public class KillPlayer : MonoBehaviour
+
+public class KIllPlayer : MonoBehaviour
 {
-    public GameObject player;
+    public GameObject Player;
     public Transform respawnPoint;
     // Start is called before the first frame update
     void Start()
@@ -20,11 +22,19 @@ public class KillPlayer : MonoBehaviour
 
     private void OnCollisionEnter2D(Collision2D other)
     {
-        Debug.Log("On collision entered");
-        if (other.gameObject.CompareTag("Player"))
+        Debug.Log("Collision occurred");
+        if(other.gameObject.CompareTag("Player"))
         {
-            player.transform.position = respawnPoint.position;
-            Debug.Log("On collision entered");
+            Debug.Log("Player Detected");
+            //***** I experimented with resetting the scene, but the progress with destorying bricks is erased.
+                // Scene currentScene = SceneManager.GetActiveScene();
+                // SceneManager.LoadScene(currentScene.name);
+            Player.transform.position = respawnPoint.position;
+            // random direction for x with vector y always going down. COPIED FROM BallMovement Script
+            float randX = Random.Range(-1.0f, 1.0f);
+            Vector2 startForce = new Vector2(randX, -1);
+            Player.GetComponent<Rigidbody2D>().velocity = startForce;
+            Player.GetComponent<Rigidbody2D>().AddForce(startForce.normalized * 400f);
         }
     }
 }
